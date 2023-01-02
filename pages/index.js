28895dbe34e09a600a58a1a617d66b4555bff148
DESKTOP-ILPQD8P\Admin <priyanshiraval26@gmail.com>
2023-01-02 21:52:05 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,16 @@ import First from '../components/First';
 import Second from '../components/Second';
 import Footer from '../components/Footer';
 import Third from '../components/Third';
-// import {
-//     BrowserRouter as Router,
-//     Switch,
-//     Route,
-//     Redirect,
-//   } from "react-router-dom";
+
 
 export default function Home() {
     return(
-        // <Router>
-        // <Switch>
         <div>
-        {/* <Route exact path="/" component={First} /> */}
             <First />
             {/* <Second />
             <Third />
             <Footer /> */}
         </div>
-        // </Switch>
-        // </Router>
+       
     )
 }
